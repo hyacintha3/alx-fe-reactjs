05130452d9b15aba31da16b7d9caafe1e4d6c88d
Hyacintha NIYONIZEYE <niyohyacintha@gmail.com>
2026-01-17 14:34:54 +0200

    refactor prop drillig to use context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,41 @@
+// import WelcomeMessage from './components/WelcomeMessage';
+// import Header from './components/Header';
+// import MainContent from './components/MainContent';
+// import Footer from './components/Footer';
+// import UserProfile from './components/UserProfile';
+
+// function App() {
+//     return (
+//         <div>
+//              <WelcomeMessage /> 
+//             <Header />
+//             <MainContent />
+//             <Footer />
+//             <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+// <UserProfile name="Bob" age={30} bio="Enjoys cooking and gaming" />
+
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
+
