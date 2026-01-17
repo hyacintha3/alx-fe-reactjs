09855ec31f7b04c1683a602381530c39c5361b76
Hyacintha NIYONIZEYE <niyohyacintha@gmail.com>
2026-01-17 15:32:04 +0200

    nice code

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,12 +1,16 @@
-const UserProfile = (props) => {
-    return (
-      <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px", borderRadius: "8px" }}>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+// src/UserProfile.jsx
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext); // <-- consume the context
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
